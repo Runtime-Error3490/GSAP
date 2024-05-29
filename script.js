879ab8139c6805fe7cce6b018e4c2897ecfd7588
Runtime-Error3490 <shaleen.shukla@learner.manipal.edu>
2024-05-30 01:21:38 +0530

    repeat and yoyo and stagger

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-gsap.to("#box1", {
-  x: 200,
-  duration: 2,
-  delay:1,
-  rotate:360,
-  ease: "bounce",
-});
-gsap.to("#box2", {
-    x: 200,
-    duration: 2,
-    delay:1,
-    backgroundColor:"red",
-    borderRadius:"15%",
-    ease: "ease-in-out",
-    });
+// gsap.to("#box1", {
+//   x: 200,
+//   duration: 2,
+//   delay:1,
+//   rotate:360,
+//   ease: "bounce",
+// });
+// gsap.to("#box2", {
+//     x: 200,
+//     duration: 2,
+//     delay:1,
+//     backgroundColor:"red",
+//     borderRadius:"15%",
+//     ease: "ease-in-out",
+//     });
 //to initial to final
 //from final to initial
+gsap.from("h1",{
+    color:"red",
+    duration:2,
+    Delay:1,
+    ease:"bounce",
+    stagger:1,
+    repeat:-1,
+    yoyo:true,
+})
