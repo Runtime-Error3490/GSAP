d0881f8bb542b388c1920a88c2e42737e9f36c6d
Runtime-Error3490 <shaleen.shukla@learner.manipal.edu>
2024-05-30 01:03:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+gsap.to("#box1", {
+  x: 200,
+  duration: 2,
+  delay:1,
+  rotate:360,
+  ease: "bounce",
+});
+gsap.to("#box2", {
+    x: 200,
+    duration: 2,
+    delay:1,
+    backgroundColor:"red",
+    borderRadius:"15%",
+    ease: "ease-in-out",
+    });
+//to initial to final
+//from final to initial
