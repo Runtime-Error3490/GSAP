5175b3bca4b5fd2ac4471ab6c3aaf3c6b1ffc17d
Runtime-Error3490 <shaleen.shukla@learner.manipal.edu>
2024-05-30 01:31:00 +0530

    timeline

--- script.js
+++ script.js
@@ -15,12 +15,39 @@
 //     });
 //to initial to final
 //from final to initial
-gsap.from("h1",{
-    color:"red",
+// gsap.from("h1",{
+//     color:"red",
+//     duration:2,
+//     Delay:1,
+//     ease:"bounce",
+//     stagger:1,
+//     repeat:-1,
+//     yoyo:true,
+// })
+
+
+var tl=gsap.timeline()
+
+tl.to("#box1",{
+    x:200,
     duration:2,
-    Delay:1,
+    delay:1,
+    rotate:360,
     ease:"bounce",
-    stagger:1,
-    repeat:-1,
-    yoyo:true,
+})
+tl.to("#box2",{
+    x:200,
+    duration:2,
+    delay:1,
+    backgroundColor:"red",
+    borderRadius:"15%",
+    ease:"ease-in-out",
+})
+tl.to("#box3",{
+    x:200,
+    duration:2,
+    delay:1,
+    backgroundColor:"green",
+    borderRadius:"15%",
+    ease:"ease-in-out",
 })
