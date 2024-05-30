186fc766d190db457a282c72630acd519a312ac9
Runtime-Error3490 <shaleen.shukla@learner.manipal.edu>
2024-05-30 13:13:21 +0530

    pin and translation in X effect

--- second/script.js
+++ second/script.js
@@ -1,30 +1,45 @@
-gsap.from("#page1 .box",{
-    scale:0,
-    duration: 2,
-    delay:1,
-    rotate:360,
-    ease: "bounce"
-})
-gsap.to("#page2 .box",{
-    scale:0,
-    duration: 2,
-    delay:1,
-    rotate:360,
-    ease: "bounce",
-    scrollTrigger:{
-        trigger:"#page2 .box",
-        scroller:"body",
-        markers:true,
-        start:"top 20%",
-        scrub:5,
-    }
-})
-gsap.from("#page3",{
-    scale:0,
-    duration: 2,
-    delay:1,
-    rotate:360,
-    ease: "bounce"
-})
-//scroll delay
+// gsap.from("#page1 .box",{
+//     scale:0,
+//     duration: 2,
+//     delay:1,
+//     rotate:360,
+//     ease: "bounce"
+// })
+// gsap.to("#page2 .box",{
+//     scale:0,
+//     duration: 2,
+//     delay:1,
+//     rotate:360,
+//     ease: "bounce",
+//     scrollTrigger:{
+//         trigger:"#page2 .box",
+//         scroller:"body",
+//         markers:true,
+//         start:"top 20%",
+//         scrub:3,
+//         pin:true,
+//     }
+// })
+// gsap.from("#page3",{
+//     scale:0,
+//     duration: 2,
+//     delay:1,
+//     rotate:360,
+//     ease: "bounce"
+// })
+// //scroll delay
 
+gsap.to("#page1 h1", {
+  transform: "translateX(-100%)",
+  duration: 3,
+  delay: 1,
+  scrollTrigger: {
+    trigger: "#page1",
+    scroller: "body",
+    markers: true,
+    scrub: 3,
+    start: "top -1%",
+    end: "bottom 20%",
+    pin: true,
+  },
+});
